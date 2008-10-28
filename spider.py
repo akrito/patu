3eeef7d884305b39613fc6be0a5b6dfb923475ec
Alex Kritikos <alex@archbook.localdomain>
2008-10-27 23:57:11 -0500

    Close more cleanly

--- spider.py
+++ spider.py
@@ -108,13 +108,13 @@ def test(options, args):
                     # remember what url referenced this link
                     queued_urls[link] = url
                     task_queue.put(link)
-        
-        for i in range(options.spiders):
-            task_queue.put('STOP')
     except KeyboardInterrupt:
         pass
     finally:
+        for i in range(options.spiders):
+            task_queue.put('STOP')
         for p in processes:
+            p.terminate()
             p.join()
     print
 
