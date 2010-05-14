dc5ab8d40397f4b9f3ef05c39f980eca9c397d04
Alex Kritikos <alex@8bitb.us>
2010-05-14 11:45:01 -0500

    HTML is pretty. Output it when testing

--- test/coverage.sh
+++ test/coverage.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-nosetests --with-coverage --cover-package=patu --cover-erase "$@"
+html_dir=`dirname $0`/cover
+nosetests --with-coverage --cover-package=patu --cover-erase --cover-html --cover-html-dir=$html_dir "$@"
